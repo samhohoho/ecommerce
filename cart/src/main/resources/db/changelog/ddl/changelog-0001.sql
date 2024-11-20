80ceeff97f2b2f2ff46504a5f56786f07067955b
Samhoho <samhoho998@gmail.com>
2024-11-20 23:12:13 +0800

    liquidbase to setup cart_item table

---
+++ cart/src/main/resources/db/changelog/ddl/changelog-0001.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+
+--changeset samhoho:1
+CREATE TABLE cart_item (
+    customer_id VARCHAR(255) NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    created_by VARCHAR(255),
+    created_on TIMESTAMP(6),
+    last_modified_by VARCHAR(255),
+    last_modified_on TIMESTAMP(6),
+    CONSTRAINT pk_cart_item PRIMARY KEY (customer_id, product_id)
+);
