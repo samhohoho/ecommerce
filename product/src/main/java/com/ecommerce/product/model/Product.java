138d270acc8a414a7565aaa4d652f3a69eebae70
Samhoho <samhoho998@gmail.com>
2025-01-01 18:55:34 +0800

    [product] setup product model

---
+++ product/src/main/java/com/ecommerce/product/model/Product.java
@@ -0,0 +1,17 @@
+package com.ecommerce.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    private Long id;
+    private String name;
+    private String shortDescription;
+    private String description;
+}
