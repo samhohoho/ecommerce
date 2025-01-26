f0401ea7232f9258c5abec7bb9e223dcf31ed548
Samhoho <samhoho998@gmail.com>
2025-01-26 21:59:55 +0800

    [cart] add openfeign dep

--- cart/src/main/java/com/ecommerce/cart/CartApplication.java
+++ cart/src/main/java/com/ecommerce/cart/CartApplication.java
@@ -2,8 +2,12 @@ package com.ecommerce.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(
+		basePackages = "com.ecommerce.cart.service"
+)
 public class CartApplication {
 
 	public static void main(String[] args) {
