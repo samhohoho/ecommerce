5fc6a0e6fadefb878218e2bc3ca51a9f43e32bed
Samhoho <samhoho998@gmail.com>
2025-01-30 14:18:17 +0800

    [product] setup branch and category table

--- product/src/main/resources/db/changelog/ddl/changelog-0001.sql
+++ product/src/main/resources/db/changelog/ddl/changelog-0001.sql
@@ -2,9 +2,24 @@
 
 --changeset samhoho:1
 CREATE TABLE product (
-    id BIGSERIAL NOT NULL,
-    name VARCHAR(255),
+    id SERIAL NOT NULL,
+    name VARCHAR(255) UNIQUE,
     description VARCHAR(255),
     short_description VARCHAR(255),
+    price FLOAT(53),
+    brand_id SERIAL,
+    category_id SERIAL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE brand (
+    id SERIAL NOT NULL,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE category (
+    id SERIAL NOT NULL,
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
